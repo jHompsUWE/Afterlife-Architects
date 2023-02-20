7061fe37e3b0e085ae1e7eb233aa87378d842f0a
Awse Noori <Awse2.Noori@live.uwe.ac.uk>
2023-02-20 12:52:40 +0000

    More orthographic setup for movement

--- Scarle2021/Game/OrthographicCamera.h
+++ Scarle2021/Game/OrthographicCamera.h
@@ -4,9 +4,6 @@
 class OrthographicCamera : public GameObject
 {
 public:
-	//near and far values exist as well, -1 for near value and 1 for far are default
-	//which is fine for an orthographic camera
-
 	OrthographicCamera(float left, float right, float bottom, float top, float near, float far);
 	~OrthographicCamera();
 
@@ -21,11 +18,16 @@ public:
 	void SetPosition(const Vector3& position) { _position = position; void RecalculateViewMatrix(); }
 	const Vector3& GetPosition() const { return _position; }
 
-	//not including rotation here, seems unnecessary for project
+	//not including rotation here not necessary
 
 	const Matrix& GetProjectionMatrix() const { return projection_matrix; }
 	const Matrix& GetViewMatrix() const { return view_matrix; }
 	const Matrix& GetViewProjectionMatrix() const { return view_projection_matrix; }
+
+	XMFLOAT3 camera_position = XMFLOAT3(0.0f, 0.0f, -5.0f);
+	XMFLOAT3 camera_target = XMFLOAT3(0.0f, 0.0f, 0.0f);
+	XMFLOAT3 camera_up = XMFLOAT3(0.0f, 1.0f, 0.0f);
+
 
 private:
 	void RecalculateViewMatrix();
@@ -33,6 +35,7 @@ private:
 	float width;
 	float height;
 
+	//values possibly work for creating ortographic perspective
 	float left = -10.0f;
 	float right = 10.0f;
 	float bottom = -10.0f;
@@ -42,7 +45,9 @@ private:
 
 protected:
 	XMMATRIX projection_matrix = XMMatrixOrthographicOffCenterLH(left, right, bottom, top, near_plane, far_plane);
-	XMMATRIX view_matrix = XMMatrixIdentity();
+	XMMATRIX view_matrix = XMMatrixLookAtLH(XMLoadFloat3(&camera_position), XMLoadFloat3(&camera_target), XMLoadFloat3(&camera_up));
+
+	//XMMATRIX view_matrix = XMMatrixIdentity();
 	XMMATRIX transformation_matrix = projection_matrix * view_matrix;
 	//Matrix projection_matrix;
 	//Matrix view_matrix;
@@ -53,4 +58,5 @@ protected:
 	Vector3 target;
 	Vector3 up;
 };
+
 
